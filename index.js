d46e69e2bcb0ea3080ddee1c6a367ca74109d45a
Selamawit  Habtemariam <selamseif@gmail.com>
2018-03-05 06:19:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
    katzDeliLine.push(name);
-   return "Welcome, " + name + "." + "  you are number " + katzDeliLine.length + "in line." 
+   return "Welcome, " + name + "." + "  you are number " + katzDeliLine.length + "in line."; 
   
 }
 takeANumber(katzDeliLine, "Ada");
